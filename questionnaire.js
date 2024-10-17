3054ad6d92732c7069bcb98bae13e7cf123e31df
isthatfar <farhan.b@hotmail.co.uk>
2024-10-17 17:36:14 +0100

    Update questionnaire.js

--- questionnaire.js
+++ questionnaire.js
@@ -1,6 +1,3 @@
-// Integrate the Frontend with Firestore
-document.getElementById('submitForm').addEventListener('click', submitFormData);
-
 // STEPS
 // Functions to navigate between steps
 function goToStep1() {
